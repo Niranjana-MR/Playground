5b3de1b8bbc87d67abeba3fe37825e10e7054f44
Niranjana M R <niranjan.mr99@gmail.com>
2021-05-31 18:44:38 +0530

    checking divisibility, implemented using python

--- Divisibility/solution.py
+++ Divisibility/solution.py
@@ -1,3 +1,4 @@
+#Accepting Inputs
 arrSize = int(input())
 numbers = input()
 arrItems = numbers.split(" ")
@@ -5,10 +6,12 @@ arrItems = numbers.split(" ")
 lastDigit = 0
 currentNumber = 0
 
+#Remove last digits of given numbers and add them up. Example: 25 45 32 >>> 552 (last digits only).
 for num in arrItems:
     lastDigit = int(num)%10
     currentNumber = (currentNumber*10) + lastDigit
 
+#check if the new formed number is divisible by 10
 if(currentNumber%10 == 0):
     print("Yes")
 else:
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is a repository where I practice and learn coding.
 
 I make use of questions from HackerEarth > https://www.hackerearth.com/
 
-NOTE: As of now, I'm just focusing on learning JavaScript. Hence, the solutions to the challenges will be in JavaScript only. 
+The solution to the challenges maybe in different languages as this is more focused on problem solving and logic over language proficiency.
